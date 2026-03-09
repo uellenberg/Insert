5d8df9b5da0fd4912f80dfb82f2602c9b8033471
uellenberg <JonahUellenberg@hotmail.com>
2026-03-09 01:54:11 -0700

    Add user input to pong
    This allows controlling the paddle with "w" for up and "s" for down. I
    modified quineRun.sh so that it sends the last received character to
    main.c every frame.

--- quineRun.sh
+++ quineRun.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+stty -echo -icanon
+trap 'stty sane' EXIT
+
 while true
 do
   clear
   cat main.c
+  gcc main.c -o a.out
 
-  gcc main.c
-  ./a.out > main.c
-  sleep 0.1
+  key=""
+  while IFS= read -r -t 0.1 -n 1 ch; do key="$ch"; done
+
+  ./a.out $key > main.c
 done
